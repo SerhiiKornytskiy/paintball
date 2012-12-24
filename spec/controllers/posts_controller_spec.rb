38667fa728b76b6c9fff37e30b2f2e02bc054b99
protectr <funkymail@list.ru>
2012-12-24 21:47:32 +0300

    controller tests added 2

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -30,7 +30,7 @@ describe PostsController do
     it "shows single post" do
       post = FactoryGirl.create(:post)
       get :show, id: post
-      assigns(:post).should eq([post])
+      assigns(:post).should eq(post)
     end	
   end
 
@@ -81,15 +81,61 @@ describe PostsController do
     it "denies access for regular user" do
       @user = FactoryGirl.create(:regular_user)
       sign_in(@user)
-      @post = FactoryGirl.create(:post)
-      lambda do
-        delete :destroy, id:  @post
-      end.should change(Post, :count).by(0)
-        response.should redirect_to(posts_path)
+      @post = FactoryGirl.create(:another_post)
+      delete :destroy, id:  @post
+      response.should redirect_to(posts_path)
     end  
 
-
-
+    it "denies access for unsigned user" do
+      @post = FactoryGirl.create(:another_post)
+      delete :destroy, id:  @post
+      response.should redirect_to(new_user_session_path)
+    end  
   end
+
+  describe "GET edit" do
+    describe "failure" do 
+      it "redirects unsigned user" do
+        get :edit, id: 34
+        response.should redirect_to(new_user_session_path) 
+      end
+
+      it "redirects unsigned user" do
+        get :edit, id: 34
+        response.should redirect_to(new_user_session_path) 
+      end
+    end  
+  end 
+
+
+  describe "PUT #update" do
+    describe "failure" do
+
+      it "denies unsigned user" do
+        put :edit, id: 3
+        response.should redirect_to(new_user_session_path)
+      end
+      
+      it "denies regular user" do
+        @regular_user = FactoryGirl.create(:regular_user)
+        sign_in(@regular_user)
+        @post = FactoryGirl.attributes_for(:post)
+        put :edit, id: @post, post: @post
+        response.should redirect_to(root_path)
+      end
+
+    end  
+
+    describe "success" do 
+      it "updates post for admin" do
+        @user = FactoryGirl.create(:user)
+        sign_in(@user)
+        @post = FactoryGirl.create(:post)
+        @post.update_attributes(title: "888")
+        @post.title.should == "888"
+      end
+
+    end  
+  end  
 
 end
